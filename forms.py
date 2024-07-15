e60dce522bd55cc83e287f6c988593e15ae71ff9
ifj123 <131675479+ifj123@users.noreply.github.com>
2024-07-15 21:18:48 +0300

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,29 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
-from .models import Profile
+from .models import Order
 
-class SupplierEditForm(forms.ModelForm):
+class OrderCreateForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['profile_picture']
-
-# forms.py
-
-from django import forms
-from django.contrib.auth.models import User
-from .models import Profile
-
-class FarmerEditForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['profile_picture']
+        model = Order
+        fields = ['farmer']
